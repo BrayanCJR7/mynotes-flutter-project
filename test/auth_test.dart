99928904e4f72c6782209c07f1791d13d72a33b6
brayancjr7 <bjimenez@threan.com>
2023-05-23 13:22:23 -0500

    Writing Notes to Cloud Firestore

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -29,7 +29,7 @@ void main() {
 
     test(
       'Should be able to initialize in less than 2 seconds',
-          () async {
+      () async {
         await provider.initialize();
         expect(provider.isInitialized, true);
       },
@@ -117,7 +117,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: 'foo@bar.com');
+    const user = AuthUser(
+      id: 'my_id',
+      isEmailVerified: false,
+      email: 'foo@bar.com',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -135,7 +139,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: 'foo@bar.com');
+    const newUser = AuthUser(
+      id: 'my_id',
+      isEmailVerified: true,
+      email: 'foo@bar.com',
+    );
     _user = newUser;
   }
 }
